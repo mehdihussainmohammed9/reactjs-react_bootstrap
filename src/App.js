d960d4862d5e20d73be9d6d2f9bc171e78f61919
Hussain9 <hussain@databeat.io>
2021-05-19 13:35:01 +0530

    sample api call

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'react-bootstrap';
- 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="primary">Primary</Button>{' '}
+import React, { Component } from "react";
+import "./App.css";
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showForm: false,
+      dataSource: [],
+
+      active: false
+    };
+  }
+  load() {
+    fetch(
+      "https://pixabay.com/api/?key=17116025-59af6bf08512e84d50c227107&q=yellow+flowers&image_type=photo&pretty=true"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({ dataSource: res.hits });
+      })
+      .catch((e) => console.log(e));
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <button onClick={() => this.load()}>click</button>
+
+        <div className="row">
+          {this.state.dataSource.map((v, i) => {
+            return (
+              <div className="col-md-4">
+                <div
+                  className="App"
+                  onClick={() => {
+                    alert("sdf");
+                  }}
+                >
+                  <div className="contacts">
+                    <h5>ID: {v.id}</h5>
+                    <p>Type: {v.type}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+
+          <div className="col-md-4"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
